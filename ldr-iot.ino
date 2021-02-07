3607e30e77d82e142f0a856da74d0259f4dc8610
moraesjeremias <jeremias@volanty.com>
2021-02-07 02:43:45 -0300

    Adding connection parameters

--- ldr-iot.ino
+++ ldr-iot.ino
@@ -1,5 +1,20 @@
+#include "ldr_secrets.h"
+
+#include <ESP8266WiFi.h>
+
+
+char wifi_ssid[]       = WIFI_USER;
+char wifi_password[]   = WIFI_PSWD;
+char aws_endpoint[]    = AWS_ENDPOINT;
+char aws_key[]         = AWS_ACCESS_KEY;
+char aws_secret[]      = AWS_SECRET_KEY;
+char aws_region[]      = AWS_REGION;
+const char* aws_topic  = AWS_THING_SHADOW;
+int port = MQTT_PORT;
+
 int ldrInitialReadValue;
 int ldrFinalReadValue;
+bool hasCarArrived;
 
 void setup() {
   Serial.begin(9600);
