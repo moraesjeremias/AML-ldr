c546d61e983f4f17b6821082d32fa30903ebf804
Jeremias Moraes <jeremias@volanty.com>
2021-02-19 01:34:51 -0300

    Adding Tanarais NTP client and adding JSON Pub/Sub msg serialization

--- ldr-iot.ino
+++ ldr-iot.ino
@@ -5,138 +5,167 @@
 #include <PubSubClient.h>
 #include <NTPClient.h>
 #include <WiFiUdp.h>
+#include <ArduinoJson.h>
 
 int ldrInitialReadValue;
 int ldrFinalReadValue;
+int timezone = -3;
 bool hasCarArrived = false;
-const char* pubSubSuccessMessage = "{\n\t\"sender\": \"esp8266-publisher\",\n\t\"hasCarArrived\": true\n}";
 
 WiFiUDP ntpUDP;
-NTPClient timeClient(ntpUDP, "pool.ntp.org");
+NTPClient timeClient(ntpUDP, "a.st1.ntp.br", timezone * 3600);
 WiFiClientSecure secureWiFiEspClient;
 
-void callback(char* topic, byte* payload, unsigned int length) {
-  Serial.print("Message arrived [");
-  Serial.print(topic);
-  Serial.print("] ");
-  for (int i = 0; i < length; i++) {
-    Serial.print((char)payload[i]);
-  }
-  Serial.println();
-
+void callback(char *topic, byte *payload, unsigned int length)
+{
+    Serial.print("Message arrived [");
+    Serial.print(topic);
+    Serial.print("] ");
+    for (int i = 0; i < length; i++)
+    {
+        Serial.print((char)payload[i]);
+    }
+    Serial.println();
 }
 
-
-void setup_wifi() {
-  secureWiFiEspClient.setBufferSizes(512, 512); 
-  WiFi.begin(WIFI_USER, WIFI_PSWD);
-  timeClient.begin();
-  while (!timeClient.update()) {
-    timeClient.forceUpdate();
-  }
-  secureWiFiEspClient.setX509Time(timeClient.getEpochTime());
+void setup_wifi()
+{
+    secureWiFiEspClient.setBufferSizes(512, 512);
+    WiFi.begin(WIFI_USER, WIFI_PSWD);
+    timeClient.begin();
+    while (!timeClient.update())
+    {
+        timeClient.forceUpdate();
+    }
+    secureWiFiEspClient.setX509Time(timeClient.getEpochTime());
 }
 
 PubSubClient client(AWS_ENDPOINT, MQTT_PORT, callback, secureWiFiEspClient);
 
-void reconnect() {
-  while (!client.connected()) {
-    if (client.connect("ESPthing")) {
-      Serial.println("connected");
-      client.publish(AWS_IOT_CORE_STATUS_CHECK_TOPIC, "Back online - ESP 8266 connected");
-    } else {
-      Serial.print("failed, rc=");
-      Serial.print(client.state());
-      Serial.println(" try again in 5 seconds");
+void reconnect()
+{
+    while (!client.connected())
+    {
+        if (client.connect("ESPthing"))
+        {
+        }
+        else
+        {
+            Serial.print("failed, rc=");
+            Serial.print(client.state());
+            Serial.println(" try again in 5 seconds");
 
-      char buf[256];
-      secureWiFiEspClient.getLastSSLError(buf, 256);
-      Serial.print("WiFiClientSecure SSL error: ");
-      Serial.println(buf);
-      delay(5000);
+            char buf[256];
+            secureWiFiEspClient.getLastSSLError(buf, 256);
+            Serial.print("WiFiClientSecure SSL error: ");
+            Serial.println(buf);
+            delay(5000);
+        }
     }
-  }
 }
 
-void setup() {
-  Serial.begin(9600);
-  Serial.begin(115200);
-  Serial.setDebugOutput(true);
+void setup()
+{
+    Serial.begin(9600);
+    Serial.setDebugOutput(true);
 
-//   setup_wifi(secureWiFiEspClient, timeClient);
-  setup_wifi();
-  delay(1000);
-  if (!SPIFFS.begin()) {
-    Serial.println("Failed to mount file system");
-    return;
-  }
+    setup_wifi();
+    delay(1000);
+    if (!SPIFFS.begin())
+    {
+        Serial.println("Failed to mount file system");
+        return;
+    }
 
-  Serial.print("Heap: "); Serial.println(ESP.getFreeHeap());
+    Serial.print("Heap: ");
+    Serial.println(ESP.getFreeHeap());
 
-  
-  File cert = SPIFFS.open("/cert.der", "r");
-  if (!cert) {
-    Serial.println("Failed to open cert file");
-  }
-  else
-    Serial.println("Success to open cert file");
+    File cert = SPIFFS.open("/cert.der", "r");
+    if (!cert)
+    {
+        Serial.println("Failed to open cert file");
+    }
+    else
+        Serial.println("Success to open cert file");
 
-  delay(1000);
+    delay(1000);
 
-  // Load private key file
-  File private_key = SPIFFS.open("/key.der", "r");
-  if (!private_key) {
-    Serial.println("Failed to open private cert file");
-  }
-  else
-    Serial.println("Success to open private cert file");
+    // Load private key file
+    File private_key = SPIFFS.open("/key.der", "r");
+    if (!private_key)
+    {
+        Serial.println("Failed to open private cert file");
+    }
+    else
+        Serial.println("Success to open private cert file");
 
-  delay(1000);
+    delay(1000);
 
-  // Load CA file
-  File ca = SPIFFS.open("/rootCA.der", "r");
-  if (!ca) {
-    Serial.println("Failed to open ca ");
-  }
-  else
-    Serial.println("Success to open ca");
+    // Load CA file
+    File ca = SPIFFS.open("/rootCA.der", "r");
+    if (!ca)
+    {
+        Serial.println("Failed to open ca ");
+    }
+    else
+        Serial.println("Success to open ca");
 
-  delay(1000);
+    delay(1000);
 
-  if (secureWiFiEspClient.loadCertificate(cert))
-    Serial.println("cert loaded");
-  else
-    Serial.println("cert not loaded");
+    if (secureWiFiEspClient.loadCertificate(cert))
+        Serial.println("cert loaded");
+    else
+        Serial.println("cert not loaded");
 
-  if (secureWiFiEspClient.loadPrivateKey(private_key))
-    Serial.println("private key loaded");
-  else
-    Serial.println("private key not loaded");
+    if (secureWiFiEspClient.loadPrivateKey(private_key))
+        Serial.println("private key loaded");
+    else
+        Serial.println("private key not loaded");
 
-  if (secureWiFiEspClient.loadCACert(ca))
-    Serial.println("ca loaded");
-  else
-    Serial.println("ca failed");
+    if (secureWiFiEspClient.loadCACert(ca))
+        Serial.println("ca loaded");
+    else
+        Serial.println("ca failed");
 
-  Serial.print("Heap: "); Serial.println(ESP.getFreeHeap());
+    Serial.println("connected");
+    client.publish(AWS_IOT_CORE_STATUS_CHECK_TOPIC, "Back online - ESP 8266 connected");
 }
 
-void loop() {
-  if (!client.connected()) {
-    reconnect();
-  }
-  client.loop();
+char *jsonPubSubMessageSerialize(bool carState, String zonedDateTime)
+{
+    char serializedPubSubMessage[512];
+    StaticJsonDocument<96> pubSubJsonSerializable;
+    pubSubJsonSerializable["sender"] = "esp8266-publisher";
+    pubSubJsonSerializable["hasCarArrived"] = carState;
+    pubSubJsonSerializable["timeArrived"] = zonedDateTime;
+    serializeJson(pubSubJsonSerializable, serializedPubSubMessage);
+    return serializedPubSubMessage;
+}
 
-  ldrInitialReadValue = analogRead(A0);
-  delay(1000);
-  ldrFinalReadValue = analogRead(A0);
+void publishMessageWhenCarArrives(int ldrInitialReadValue, int ldrFinalReadValue)
+{
+    if (ldrFinalReadValue - ldrInitialReadValue > 100)
+    {
+        hasCarArrived = true;
+        char *pubSubMessage = jsonPubSubMessageSerialize(hasCarArrived, timeClient.getFormattedDate());
+        Serial.print(pubSubMessage);
+        client.publish(AWS_IOT_CORE_TOPIC, pubSubMessage);
+    }
+}
 
-  if (ldrFinalReadValue - ldrInitialReadValue > 100) {
-    hasCarArrived = true;
-    Serial.print("Object is approaching");
-    Serial.print("\n");
-    Serial.print(hasCarArrived);
-    client.publish(AWS_IOT_CORE_TOPIC, pubSubSuccessMessage);
-  }
-  delay(100);
+void loop()
+{
+    if (!client.connected())
+    {
+        reconnect();
+    }
+
+    ldrInitialReadValue = analogRead(A0);
+    delay(1000);
+    ldrFinalReadValue = analogRead(A0);
+    publishMessageWhenCarArrives(ldrInitialReadValue, ldrFinalReadValue);
+
+    delay(100);
+    client.loop();
+    
 }
